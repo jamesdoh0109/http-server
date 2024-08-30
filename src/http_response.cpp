ddf5006c324c07398e8a3a07f662d03357b08b7d
Vikas Choudhary <vikaschoudharycs097@gmail.com>
2024-08-30 20:50:22 +0530

    [Doc]: Added documentation comments for HttpResponse class

--- src/http_response.cpp
+++ src/http_response.cpp
@@ -2,17 +2,36 @@
 #include "http.h"
 #include "http_response.h"
 
+/*************************************************
+ * Set the response status code.
+ * 
+ * @param: statusCode value
+ *************************************************/
 void HttpResponse::setStatusCode(HttpStatusCode statusCode)
 {
     this->statusCode = statusCode;
 }
 
+/*************************************************
+ * Set the response content and content type.
+ * 
+ * @param: content value
+ * @param: content type
+ *************************************************/
 void HttpResponse::setContent(std::string content, std::string contentType)
 {
     this->content = content;
     this->contentType = contentType;
 }
 
+/****************************************************
+ * Get the raw string representation of the response
+ * 
+ * @param: content value
+ * @param: content type
+ *
+ * @returns raw string
+ ***************************************************/
 const std::string HttpResponse::getRawResponseString(void)
 {
     std::ostringstream outputStream;
@@ -25,11 +44,19 @@ const std::string HttpResponse::getRawResponseString(void)
     return outputStream.str();
 }
 
+/****************************************************
+ * Construct OK response with given content and 
+ * content type 
+ * 
+ * @param: content value
+ * @param: content type
+ *
+ * @returns HttpResponse
+ ***************************************************/
 HttpResponse HttpResponse::getOkResponse(std::string content, std::string contentType)
 {
     HttpResponse response;
     response.setContent(content, contentType);
-    response.setStatusCode(HttpStatusCode::OK);
 
     return response;
 }
